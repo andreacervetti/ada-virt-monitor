9626becb55840a52f9f5819b9290a11bc795e988
andrea.cervetti <andrea.cervetti@gmail.com>
2019-05-08 17:11:32 +0200

    Removed Glib.Object.User_Data from migration window Derived new type from gtk_window to hold shared data

--- avm/src/gui/migration_window.adb
+++ avm/src/gui/migration_window.adb
@@ -29,7 +29,6 @@ with Gtk.Handlers;       use Gtk.Handlers;
 with Gtk.Label;          use Gtk.Label;
 with Gtk.Progress_Bar;   use Gtk.Progress_Bar;
 
-with Glib.Object;
 with Glib.Main;
 use Glib.Main;
 
@@ -48,6 +47,29 @@ with Glib; use Glib;
 
 package body Migration_Window is
    
+   -- We derive a new gtk_window type with migratio data used
+   -- by the callbacks. We cannot use global variables because
+   -- different migration windows can be open at the same time
+   type My_Window_Record is new Gtk_Window_Record with
+      record
+         Source_Host : Gtk_Label;
+         Domain      : Gtk_Label;
+         Target      : Gtk_Combo_Box_Text;
+         VM          : VM_Type;
+         Job         : Positive;
+      end record;
+   
+   type My_Window is access all My_Window_Record'Class;
+   
+   procedure Gtk_New
+      (Window   : out My_Window;
+       The_Type : Gtk.Enums.Gtk_Window_Type := Gtk.Enums.Window_Toplevel)
+   is
+   begin
+      Window := new My_Window_Record;
+      Gtk.Window.Initialize (Window, The_Type);
+   end Gtk_New;
+
    package Return_Callback is new Gtk.Handlers.Return_Callback
      (Gtk.Widget.Gtk_Widget_Record, Boolean);
 
@@ -59,46 +81,21 @@ package body Migration_Window is
    begin
       Win.Destroy;
    end Close_Window;
-      
-   type Migration_Data_Record is
-      record
-         Source_Host : Gtk_Label;
-         Domain      : Gtk_Label;
-         Target      : Gtk_Combo_Box_Text;
-      end record;
-   
-   package User_Migration_Data is
-     new Glib.Object.User_Data (Migration_Data_Record);
-   use User_Migration_Data;
-   Migration_Data : constant String := "migration_data";
-   
-   package User_Domain_Data is
-     new Glib.Object.User_Data (VM_Type);
-   use User_Domain_Data;
-   Domain_Data : constant String := "domain";
-   
-   package User_Job_Data is
-     new Glib.Object.User_Data (Positive);
-   use User_Job_Data;
-   Job_Data : constant String := "jobnumber";
-   
+                  
    package Update_Progress is new Glib.Main.Generic_Sources
      (Gtk_Progress_Bar);
    use Update_Progress;
    
    function Timed_Progress_Updater (Progress : Gtk_Progress_Bar)
                                     return Boolean is
-      Win         : Gtk_Window;
-      Job_Number  : Positive;
-      Domain      : VM_Type;
+      Win         : My_Window;
       
    begin
-      Win := Gtk_Window (Progress.Get_Ancestor (Gtk.Window.Get_Type));
-      Job_Number := User_Job_Data.Get (Win, Job_Data);
-      Domain := User_Domain_Data.Get (Win, Domain_Data);
+      Win := My_Window (Progress.Get_Ancestor (Gtk.Window.Get_Type));
+
       -- check if job is active
-      if Is_Running (Job_Number) then
-         Progress.Set_Fraction (Gdouble (Domain.Get_Job_Progress));
+      if Is_Running (Win.Job) then
+         Progress.Set_Fraction (Gdouble (Win.VM.Get_Job_Progress));
          return True;
       else
          Win.Destroy;
@@ -108,37 +105,29 @@ package body Migration_Window is
    
    procedure Start_Migration (Object : access Gtk_Widget_Record'Class) is
 
-      Win         : Gtk_Window;
+      Win         : My_Window;
       Box         : Gtk_Box;
       Label       : Gtk_Label;
       Progress    : Gtk_Progress_Bar;
-      Data        : Migration_Data_Record;
-      Domain      : VM_Type;
       To_Host     : Hypervisor;
-      Job_Number  : Positive;
       Timeout_Id  : G_Source_Id;
    begin
       -- Retrieve migration parameters stored as user_data in the
       -- window object
-      Win := Gtk_Window (Object.Get_Ancestor (Gtk.Window.Get_Type));
+      Win := My_Window (Object.Get_Ancestor (Gtk.Window.Get_Type));
 
-      Data := User_Migration_Data.Get (Win, Migration_Data);
       --no error control yet
-      Domain := Get_Domain (Data.Source_Host.Get_Text,
-                            Data.Domain.Get_Text);
-      To_Host := Get_Host (Data.Target.Get_Active_Text);
+      Win.VM  := Get_Domain (Win.Source_Host.Get_Text,
+                             Win.Domain.Get_Text);
+      To_Host := Get_Host (Win.Target.Get_Active_Text);
 
       -- Start migration
-      Job_Number := Migrate (Domain, To_Host);
-
-      -- Store the job Number end domain as User_data in the window
-      User_Job_Data.Set (Win, Job_Number, Job_Data);
-      User_Domain_Data.Set (Win, Domain, Domain_Data);
+      Win.Job := Migrate (Win.VM, To_Host);
       
       -- Set a label with the migration message
       -- we must set it before raplacing the content of the window
       Gtk.Label.Gtk_New (Label);
-      Label.Set_Text ("Migrating " & Data.Domain.Get_Text);
+      Label.Set_Text ("Migrating " & Win.Domain.Get_Text);
 
       -- get the box container of the top level window
       Box := Gtk_Box (Win.Get_Child);
@@ -162,7 +151,7 @@ package body Migration_Window is
    procedure Create_Migration_Window
      (Server_Name : String; Domain_Name : String) is
      
-      Window         : Gtk_Window;
+      Window         : My_Window;
       Box            : Gtk_Box;
       Grid           : Gtk_Grid;
       Label          : Gtk_Label;
@@ -170,8 +159,8 @@ package body Migration_Window is
       Migrate_Button : Gtk_Button;
       Cancel_Button  : Gtk_Button;
       
-      Data           : Migration_Data_Record;
-      
+      Target         : Gtk_Combo_Box_Text;
+            
       Unique_Name    : String := 
         "domain:" & Server_Name & "/" & Domain_Name & "/migration";
             
@@ -189,11 +178,11 @@ package body Migration_Window is
            List_Servers (Get_Group (Group (Get_Host (Server_Name))));
          Count : Natural := 0;
       begin
-         Gtk.Combo_Box_Text.Gtk_New (Data.Target);
+         Gtk.Combo_Box_Text.Gtk_New (Target);
          for I in Host_List'Range loop
             if Host_List (I).Server_Name /= Server_Name
               and then Host_List (I).Is_Connected then
-               Data.Target.Append_Text (Host_List (I).Server_Name);
+               Target.Append_Text (Host_List (I).Server_Name);
                Count := Count + 1;
             end if;
          end loop;
@@ -206,10 +195,11 @@ package body Migration_Window is
                     Buttons     => Button_OK);
             return;
          end if;
-         Data.Target.Set_Active (0);
+         Target.Set_Active (0);
       end;
 
-      Gtk.Window.Gtk_New (Window);
+      Gtk_New (Window);
+      Window.Target := Target;
       Window.Set_Name (Unique_Name);
 --        Window.Set_Default_Size (400, 300);
       Window.Set_Title ("Migrate " & Domain_Name);
@@ -225,23 +215,23 @@ package body Migration_Window is
             
       Gtk.Label.Gtk_New (Label, "Migrate Domain :");
       Label.Set_Halign (Align_End);
-      Gtk.Label.Gtk_New (Data.Domain, Domain_Name);
-      Data.Domain.Set_Halign (Align_Start);
+      Gtk.Label.Gtk_New (Window.Domain, Domain_Name);
+      Window.Domain.Set_Halign (Align_Start);
       Grid.Attach_Next_To (Label, null, Pos_Bottom);
-      Grid.Attach_Next_To (Data.Domain, Label, Pos_Right);
+      Grid.Attach_Next_To (Window.Domain, Label, Pos_Right);
 
       Gtk.Label.Gtk_New (Label, "From Host :");
       Label.Set_Halign (Align_End);
-      Gtk.Label.Gtk_New (Data.Source_Host, Server_Name);
-      Data.Source_Host.Set_Halign (Align_Start);
+      Gtk.Label.Gtk_New (Window.Source_Host, Server_Name);
+      Window.Source_Host.Set_Halign (Align_Start);
       Grid.Attach_Next_To (Label, null, Pos_Bottom);
-      Grid.Attach_Next_To (Data.Source_Host, Label, Pos_Right);
+      Grid.Attach_Next_To (Window.Source_Host, Label, Pos_Right);
       
       Gtk.Label.Gtk_New (Label, "To Host :");
       Label.Set_Halign (Align_End);
-      Data.Target.Set_Halign (Align_Start);
+      Window.Target.Set_Halign (Align_Start);
       Grid.Attach_Next_To (Label, null, Pos_Bottom);
-      Grid.Attach_Next_To (Data.Target, Label, Pos_Right);
+      Grid.Attach_Next_To (Window.Target, Label, Pos_Right);
       
       
       Gtk.Button_Box.Gtk_New (Buttons, Orientation_Horizontal);
@@ -260,9 +250,6 @@ package body Migration_Window is
          "clicked",
          Widget_Callback.To_Marshaller (Start_Migration'Access));
       
-      -- Store the migration parameter in the main window as User_Data
-      User_Migration_Data.Set (Window, Data, Migration_Data);
-
       Buttons.Add (Cancel_Button);
       Buttons.Add (Migrate_Button);
       Box.Add (Buttons);
